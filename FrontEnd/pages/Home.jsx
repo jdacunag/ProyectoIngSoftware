3d3a099fcf0f3b012c93259d4373d78da1042a7e
Crissitx <camilocardenas2705@gmail.com>
2023-11-24 05:37:32 -0500

    Home Style

--- FrontEnd/pages/Home.jsx
+++ FrontEnd/pages/Home.jsx
@@ -3,16 +3,14 @@ import Card from '../components/Card'
 import style from './Home.module.css'
 import Title from '../components/title';
 import { faRightFromBracket, faShop } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import	NavbarSimple from '../components/Navbar'
 import Button from '../components/Button';
 import { useNavigate } from 'react-router-dom';
-import useSession from '../hooks/useSession';
 
 
 function Home() {
   const navigate = useNavigate();
-  const {userId} = useSession();
-  console.log(userId)
   const handlePurchase = async(e) => {
     e.preventDefault();
 
@@ -23,41 +21,56 @@ function Home() {
   };
 
   useEffect(() => {
-    
     // Realiza la solicitud GET para obtener la lista de universidades
-    fetch("http://127.0.0.1:8000/University")
+    fetch('http://127.0.0.1:8000/University')
       .then((response) => response.json())
       .then((data) => setUniversities(data))
-      .catch((error) => console.error("Error:", error));
+      .catch((error) => console.error('Error:', error));
   }, []);
   const [universities, setUniversities] = useState([]);
- 
+
   return (
     <div>
       <NavbarSimple></NavbarSimple>
-      <div className={style.container}>
+        <div className={style.container}>
         <div>
-          <Title className={style.title}>Universidades Disponibles</Title>
-        </div>
+                <Title className={style.title}> 
+
+                 Universidades Disponibles
+                 </Title>
+            </div>
         <div className={style.projects}>
-          {universities.map((uni) => (
-            <Card key={uni.nombre}>
-              <img src={uni.logo} className={style.banner} />
-              <div className={style.panel}>
-                <p>{uni.nombre}</p>
-                <p>{uni.email}</p>
+        {universities.map((uni) => (
+              <Card backgroundColor='#f0f0f0' key={uni.nombre}>
+                <img
+                  src={uni.logo}
+                  className={style.banner}
+                  style={{
+                    padding: '10px',
+                    border: '0px solid #000000',
+                    aspectRatio: '28 / 10'
+                  }}
+                />
+                <br></br><br></br>
+                <div style={{ marginLeft: '10px' }}>
+                  <Title fontSize={22}>{uni.nombre}</Title>
+                  <p style={{ marginTop: '5px' }}>{uni.email}</p>
+                </div>
+                <br></br>
                 <div>
                 <Button className={style.button} icon={faShop} onClick={handlePurchase}>Comprar información</Button>
-                </div>
                 </div>
               </Card>
             ))}
         </div>
         </div>
-        </div>
 
-    
-  );
+          
+  
+        <div>
+        </div>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
