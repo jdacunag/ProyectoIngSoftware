6dc3594c211cb04c9fc45ffab44fcd4d058d958e
julimejia <davidmejiao43@gmail.com>
2023-11-24 05:20:52 -0500

    #

--- FrontEnd/pages/Home.jsx
+++ FrontEnd/pages/Home.jsx
@@ -1,14 +1,27 @@
-import { useEffect, useState } from "react";
-import Card from "../components/Card";
-import style from "./Home.module.css";
-import Title from "../components/title";
-import { faShop } from "@fortawesome/free-solid-svg-icons";
-import NavbarSimple from "../components/Navbar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import  useSession  from "../hooks/useSession";
+import { useEffect, useState } from 'react'
+import Card from '../components/Card'
+import style from './Home.module.css'
+import Title from '../components/title';
+import { faRightFromBracket, faShop } from '@fortawesome/free-solid-svg-icons';
+import	NavbarSimple from '../components/Navbar'
+import Button from '../components/Button';
+import { useNavigate } from 'react-router-dom';
+import useSession from '../hooks/useSession';
+
 
 function Home() {
+  const navigate = useNavigate();
   const {userId} = useSession();
+  console.log(userId)
+  const handlePurchase = async(e) => {
+    e.preventDefault();
+
+    // AGREGAR AL CARRITO LA ESCUELA SELECCIONADA
+    // NO MANDAR DE UNA (OPCIONAL)
+
+    navigate('/payment');
+  };
+
   useEffect(() => {
     
     // Realiza la solicitud GET para obtener la lista de universidades
@@ -18,7 +31,7 @@ function Home() {
       .catch((error) => console.error("Error:", error));
   }, []);
   const [universities, setUniversities] = useState([]);
-  console.log(userId)
+ 
   return (
     <div>
       <NavbarSimple></NavbarSimple>
@@ -33,19 +46,17 @@ function Home() {
               <div className={style.panel}>
                 <p>{uni.nombre}</p>
                 <p>{uni.email}</p>
-
-                <button className={style.button}>
-                  <FontAwesomeIcon icon={faShop} color="red" size="x1" />
-                  <p>comprar</p>
-                </button>
-              </div>
-            </Card>
-          ))}
+                <div>
+                <Button className={style.button} icon={faShop} onClick={handlePurchase}>Comprar información</Button>
+                </div>
+                </div>
+              </Card>
+            ))}
         </div>
-      </div>
+        </div>
+        </div>
 
-      <div></div>
-    </div>
+    
   );
 }
 
