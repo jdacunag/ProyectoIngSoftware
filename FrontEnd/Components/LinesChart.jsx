c44ad642d7aaa4bbd64507834adaecf03e160fa1
Crissitx <camilocardenas2705@gmail.com>
2023-11-24 12:41:10 -0500

    Charts

---
+++ FrontEnd/Components/LinesChart.jsx
@@ -0,0 +1,73 @@
+import { Line } from 'react-chartjs-2';
+import PropTypes from 'prop-types';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler,
+} from 'chart.js';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler
+);
+
+
+
+export default function LinesChart({ valores,valores2, labels, label, label2, min, max}) {
+
+
+var midata = {
+    labels: labels,
+    datasets: [ // Cada una de las líneas del gráfico
+        {
+            label: label,
+            data: valores,
+            tension: 0.5,
+            fill : true,
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            pointRadius: 5,
+            pointBorderColor: 'rgba(255, 99, 132)',
+            pointBackgroundColor: 'rgba(255, 99, 132)',
+        },
+        {
+            label: label2,
+            data: valores2
+        },
+    ],
+};
+
+var misoptions = {
+    scales : {
+        y : {
+            max: max,
+            min: min
+        },
+        x: {
+            ticks: { color: 'rgb(255, 99, 132)'}
+        }
+    }
+};
+    return <Line data={midata} options={misoptions}/>
+}
+LinesChart.propTypes = {
+    valores: PropTypes.string.isRequired,
+    valores2: PropTypes.string,
+    labels: PropTypes.string.isRequired,
+    label2: PropTypes.string,
+    label: PropTypes.string.isRequired,
+    max: PropTypes.number.isRequired,
+    min: PropTypes.number
+}
